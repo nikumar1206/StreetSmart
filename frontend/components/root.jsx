c97914203ee09a8933b5d19d0a65a036803b97d3
nikumar1206 <nikumar1206@gmail.com>
2022-06-20 14:32:18 -0400

    uglifier update

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,7 +1,7 @@
 import { HashRouter } from "react-router-dom";
 import React from "react";
 import { Provider } from "react-redux";
-import App from "./App";
+import App from "./app";
 
 export const Root = ({ store }) => (
   <Provider store={store}>
