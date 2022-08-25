4b32b5b9d02c3387e3e1091fcb2332864cc76593
nikumar1206 <nikumar1206@gmail.com>
2022-08-24 22:17:55 -0400

    removed logger

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,10 +22,11 @@ class User < ApplicationRecord
 
     has_many :listings, class_name: "Listing", foreign_key: "lister_id"
     has_many :saves, class_name: "Save", foreign_key: "user_id"
+    has_many :notes, class_name: "Note", foreign_key: "user_id"
 
     has_many :saved_listings, through: :saves, source: :listing
 
-    has_many :notes, class_name: "Note", foreign_key: "user_id"
+
     def self.find_by_credentials(email, password)
         user = User.find_by(email: email)
         return nil if user.nil?
